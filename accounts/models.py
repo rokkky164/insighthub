78f32ca46d8f2bbe8e578d3177dc1d9a0de318b4
Roshan Hansdah <hansdah.roshan@gmail.com>
2025-09-28 10:57:07 +0530

    Mig files updted

--- accounts/models.py
+++ accounts/models.py
@@ -17,6 +17,7 @@ class User(AbstractUser):
         VIEWER = "VIEWER", "Viewer"
 
     role = CharField(max_length=20, choices=Role.choices, default=Role.VIEWER)
+    phone = CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return f"{self.username} ({self.role})"
